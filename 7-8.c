b09307c52bff0a3af3295d703d3b7667a58af209
Jing0 <j.kuo2012@gmail.com>
2014-02-10 22:39:25 +0800

    chapter7

---
+++ 7-8.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+int d1,d2,d3,d4;
+int main(){
+	int a;
+	void num_space(int);
+	printf("Please input a 4-digit number:");
+	scanf("%d",&a);
+	while(a<1000||a>9999){
+		printf("Error!\aPlease input a 4-digit number:");
+		scanf("%d",&a);
+	}
+	num_space(a);
+	printf("%d %d %d %d\n",d1,d2,d3,d4);
+}
+void num_space(int a){
+	d1=a/1000;
+	d2=(a-1000*d1)/100;
+	d3=(a-1000*d1-100*d2)/10;
+	d4=a%10;
+}
