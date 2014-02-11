4196de153d05aa9efc31f822807df2c21ed4292d
Jing0 <j.kuo2012@gmail.com>
2014-02-11 21:27:03 +0800

    chapter7

---
+++ 7-6.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+char str[200];
+int main(){
+	void connect_str(char str1[],char str2[]);
+	char str1[100],str2[100];
+	printf("Please input the first string:");
+	scanf("%s",str1);
+	printf("Please input the second string:");
+	scanf("%s",str2);
+	connect_str(str1,str2);
+	printf("The new string is %s\n",str);
+}
+void connect_str(char str1[],char str2[]){
+	int i,t;
+	for(i=0;str1[i]!='\0';i++){
+		str[i]=str1[i];
+		t=i;
+	}
+	for(i=0;str2[i]!='\0';i++)
+		str[t+i+1]=str2[i];
+}
