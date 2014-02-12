4a92133f802118a4ae1dbd4105e6507650e099d9
Jing0 <j.kuo2012@gmail.com>
2014-02-12 14:12:54 +0800

    chapter7

---
+++ 7-7.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+	void vowel(char str1[],char str2[]);
+	char str1[100];
+	char str2[100];
+	printf("Please input a character string:\n");
+	scanf("%s",str1);
+	vowel(str1,str2);
+	printf("The vowel letters in the string: %s\n",str2);
+}
+void vowel(char str1[],char str2[]){
+	int i,j=0;
+	for(i=0;str1[i]!='\0';i++){
+		if(str1[i]=='a'||str1[i]=='e'||str1[1]=='i'||str1[1]=='o'||str1[1]=='u'||str1[1]=='A'||str1[1]=='E'||str1[1]=='I'||str1[1]=='O'||str1[1]=='U'){
+			str2[j]=str1[i];
+			j=j+1;
+		}
+	}
+}
