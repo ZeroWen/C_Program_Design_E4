252b7b91bee468442cace6fad6213f7dc8613be3
Jing0 <j.kuo2012@gmail.com>
2014-02-26 20:46:27 +0800

    chapter7_1

--- 7-18.c
+++ 7-18.c
@@ -1,5 +1,34 @@
 #include <stdio.h>
 int main(){
+	int sum_day(int month,int day);
+	int leap(int year);
+	int year,month,day,days;
+	printf("input date(year,month,day):");
+	scanf("%d,%d,%d",&year,&month,&day);
+	printf("%d/%d/%d ",year,month,day);
+	days=sum_day(month,day);
+	if(leap(year)&&month>=3)
+		days=days+1;
+	printf("is the %dth day in this year.\n",days);
+	return 0;
+}
+//函数sum_day:计算日期
+int sum_day(int month,int day){
+	int day_tab[13]={0,31,28,31,30,31,30,31,31,30,31,30,31};
+	int i;
+	for(i=1;i<month;i++)
+		day+=day_tab[i];
+	return(day);
+}
+//函数leap:判断是否为闰年
+int leap(int year){
+	int leap;
+	leap=year%4==0&&year%100!=0||year%400==0;
+	return(leap);
+}
+/*
+#include <stdio.h>
+int main(){
 	int year,month,day;
 	int count_day(int,int,int);
 	printf("Please enter the year:");
@@ -34,3 +63,4 @@ int count_day(int year,int month,int day){
 	else
 		return(num);
 }
+*/
