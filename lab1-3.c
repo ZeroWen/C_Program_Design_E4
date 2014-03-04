11fd51b9f30b4f8806964763f39f5f03531dae07
Jing0 <j.kuo2012@gmail.com>
2014-03-04 22:59:46 +0800

    lab1-3
    实验一 C语言程序结构及程序运行的全过程

---
+++ lab1-3.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab1-3.c
+ *
+ *    Description:  输入5个数，求这5个输的平方和
+ *        
+ *        Version:  1.0
+ *        Created:  03/03/2014 22:09:30
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo , j.kuo2012@gmail.com
+ * =====================================================================================
+ */
+#include <stdio.h>
+int main(){
+	int x1,x2,x3,x4,x5;
+	float y;
+	printf("Please input five integer numbers:");
+	scanf("%d,%d,%d,%d,%d",&x1,&x2,&x3,&x4,&x5);
+	y=x1*x1+x2*x2+x3*x3+x4*x4+x5*x5;
+	printf("The sum of square is %5.0f\n",y);
+	return 0;
+}
