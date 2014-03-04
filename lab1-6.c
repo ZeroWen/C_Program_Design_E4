12a4f3011c066a47e27932afb6bbe63f45d437bc
Jing0 <j.kuo2012@gmail.com>
2014-03-04 23:00:15 +0800

    lab1-6
    实验一 C语言程序结构及程序运行的全过程

---
+++ lab1-6.c
@@ -0,0 +1,22 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab1-6.c
+ *
+ *    Description:  在屏幕上输出：
+ *    			"what a beautiful campus !"
+ *    			"I wish you every success !"
+ *
+ *        Version:  1.0
+ *        Created:  03/03/2014 22:40:25
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (), j.kuo2012@gmail.com
+ * =====================================================================================
+ */
+#include <stdio.h>
+int main(){
+	printf("what a beautiful campus !\nI wish you every success !\n");
+	return 0;
+}
