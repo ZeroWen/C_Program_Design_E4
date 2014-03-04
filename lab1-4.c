51e676dac5b72b78aefa7db58fc5e870e74d7647
Jing0 <j.kuo2012@gmail.com>
2014-03-04 22:59:56 +0800

    lab1-4.c
    实验一 C语言程序结构及程序运行的全过程

---
+++ lab1-4.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab1-4.c
+ *
+ *    Description:  求三个数平方根的和
+ *
+ *        Version:  1.0
+ *        Created:  03/03/2014 22:24:49
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo , j.kuo2012@gmail.com
+ * =====================================================================================
+ */
+#include <stdio.h>
+#include <math.h>
+int main(){
+	int a,b,c;
+	float s;
+	printf("Please input three numbers:");
+	scanf("%d,%d,%d",&a,&b,&c);
+	s=sqrt(a)+sqrt(b)+sqrt(c);
+	printf("The sum of square root is %f\n",s);
+	return 0;
+}
