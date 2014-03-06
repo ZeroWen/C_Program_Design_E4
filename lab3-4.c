45d23176c897a214e3206e6b64f2cbd8c4e33f39
Jing0 <j.kuo2012@gmail.com>
2014-03-07 00:38:14 +0800

    lab3-4

---
+++ lab3-4.c
@@ -0,0 +1,25 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab3-4.c
+ *
+ *    Description:  none
+ *
+ *        Version:  1.0
+ *        Created:  03/06/2014 23:24:53
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (), j.kuo2012@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(){
+	char c1,c2,c3,c4;
+	scanf("%c %c %c %c",&c1,&c2,&c3,&c4);
+	printf("%d %d %d %d",c1,c2,c3,c4);
+	return 0;
+}
