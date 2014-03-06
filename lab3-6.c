482a39c4fe337c6742c86dcb6a617e1bc099fe22
Jing0 <j.kuo2012@gmail.com>
2014-03-07 01:01:46 +0800

    lab3-6

---
+++ lab3-6.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab3-6.c
+ *
+ *    Description:  none
+ *
+ *        Version:  1.0
+ *        Created:  03/07/2014 00:29:46
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (), j.kuo2012@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(){
+	float m,n;
+	m=12345678;
+	n=3456789;
+	printf("%9.0f,%8.0f",m,n);
+	return 0;
+}
