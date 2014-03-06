465dccca0b2c24d122ee4cded9b5cc454f1e5443
Jing0 <j.kuo2012@gmail.com>
2014-03-07 00:38:09 +0800

    lab3-3

---
+++ lab3-3.c
@@ -0,0 +1,26 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab3-3.c
+ *
+ *    Description:  none
+ *
+ *        Version:  1.0
+ *        Created:  03/06/2014 23:24:41
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (), j.kuo2012@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	scanf("%d %d %d",&a,&b,&c);
+	printf("x1=a+b+c=%d+%d+%d=%d",a,b,c,a+b+c);
+	printf("x2=a-b-c=%d-%d-%d=%d",a,b,c,a-b-c);
+	return 0;
+}
