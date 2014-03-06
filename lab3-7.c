22e3ff886d70ad6a7be8ac3c731ca93150785f00
Jing0 <j.kuo2012@gmail.com>
2014-03-07 01:01:41 +0800

    lab3-7

---
+++ lab3-7.c
@@ -0,0 +1,21 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab3-7.c
+ *
+ *    Description:  none
+ *
+ *        Version:  1.0
+ *        Created:  03/07/2014 00:29:57
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo, j.kuo2012@gmail.com
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(){
+	printf("Octal:%o,%o,%o\nHex:%x,%x,%x\n",20,64,127,20,64,127);
+	return 0;
+}
