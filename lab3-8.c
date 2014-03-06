7ac06decfc185f7a7f66890ce9d001c62edcbcd3
Jing0 <j.kuo2012@gmail.com>
2014-03-07 01:01:36 +0800

    lab3-8

---
+++ lab3-8.c
@@ -0,0 +1,27 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab3-8.c
+ *
+ *    Description:  none
+ *
+ *        Version:  1.0
+ *        Created:  03/07/2014 00:30:12
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (), j.kuo2012@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main()[
+	float x,y,z;
+	x=1.414;
+	y=1.732;
+	z=2.712;
+	printf("%6.2f,y=%6.2f,z=%6.2f\n",x,y,z);
+	return 0;
+}
