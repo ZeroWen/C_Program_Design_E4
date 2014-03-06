d9219b4852d109e0be22b98b1b3c203b07b0cf68
Jing0 <j.kuo2012@gmail.com>
2014-03-07 00:38:00 +0800

    lab3-1

---
+++ lab3-1.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab3-1.c
+ *
+ *    Description:  none
+ *
+ *        Version:  1.0
+ *        Created:  03/06/2014 23:24:02
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (), j.kuo2012@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	float x,y,z;
+	a=3;
+	b=4;
+	c=5;
+	x=1.414;
+	y=1.732;
+	z=2.712;
+	printf("a=%-7d b=%-7d c=%-7d\n",a,b,c);
+	printf("x=%-7.5f y=%-7.5f z=%-7.5f\n",x,y,z);
+	return 0;
+}
