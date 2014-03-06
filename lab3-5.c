69faab0c17bdb91810cf29a32a60830f127f04b3
Jing0 <j.kuo2012@gmail.com>
2014-03-07 01:01:52 +0800

    lab3-5

---
+++ lab3-5.c
@@ -0,0 +1,28 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab3-5.c
+ *
+ *    Description:  none
+ *
+ *        Version:  1.0
+ *        Created:  03/07/2014 00:29:32
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (), j.kuo2012@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(){
+	int d1,d2,d3,d4;
+	d1=60;
+	d2=61;
+	d3=62;
+	d4=63;
+	printf("%c,%c,%c,%c",d1+5,d2+5,d3+5,d4+5);
+	return 0;
+}
