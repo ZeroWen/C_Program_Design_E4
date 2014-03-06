e6e1024efb94475bd5d59d712b8d09e0b7ab2359
Jing0 <j.kuo2012@gmail.com>
2014-03-07 00:38:05 +0800

    lab3-2

---
+++ lab3-2.c
@@ -0,0 +1,32 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  lab3-2.c
+ *
+ *    Description:  none
+ *
+ *        Version:  1.0
+ *        Created:  03/06/2014 23:24:32
+ *       Revision:  none
+ *       Compiler:  gcc
+ *
+ *         Author:  Jackie Kuo (), j.kuo2012@gmail.com
+ *   Organization:  
+ *
+ * =====================================================================================
+ */
+
+#include <stdio.h>
+int main(){
+	int a,b,c;
+	float x,y,z;
+	scanf("%d",&a);
+	scanf("%d",&b);
+	scanf("%d",&c);
+	scanf("%f",&x);
+	scanf("%f",&y);
+	scanf("%f",&z);
+	printf("a=%d,b=%d,c=%d\n",a,b,c);
+	printf("x=%f,y=%f,z=%f\n",x,y,z);
+	return 0;
+}
